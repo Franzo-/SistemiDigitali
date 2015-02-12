0412bb8b5bd01adfc0b84d10d45da5de7435a0b2
Franzo- <filippo.franzoni@studio.unibo.it>
2015-02-12 18:38:47 +0100

    Aggiunto il componente counter alla Map

--- Model/Map.vhd
+++ Model/Map.vhd
@@ -6,55 +6,70 @@ use work.pacman_package.all;
 entity MapEntity is
   port
     (
-      CLOCK : in std_logic;
+      CLOCK   : in std_logic;
       RESET_N : in std_logic;
 
       -- Query sul contenuto delle celle
-      QUERY_CELL : in cell_coordinates;
+      QUERY_CELL   : in  cell_coordinates;
       CELL_CONTENT : out map_cell_type;
 
       -- Rimuove una caramellina su richiesta del controller
-      REMOVE_CANDY : in cell_coordinates
-      
-);
-  
+      REMOVE_CANDY         : in cell_coordinates;
+      UPDATE_CANDY_COUNTER : in std_logic;
+
+      -- Valori del contatore di caramelline
+      CANDY_LEFT : out integer range 0 to (MAX_CANDIES-1);
+      SCORE      : out integer range 0 to (MAX_CANDIES-1)
+      );
+
 end entity MapEntity;
 
 architecture RTL of MapEntity is
-  signal map_board : map_type;
-  variable candy_removal : std_logic := '0';
+  signal map_board     : map_type;
+  signal candy_removal : std_logic;
 
 begin
 
+  CandyCounter : entity work.CandyCounter
+    port map (
+      RESET_N => RESET_N;
+      CLK     => CLOCK;
+      ENABLE  => UPDATE_CANDY_COUNTER;
+      COUNT   => CANDY_LEFT;
+      SCORE   => SCORE
+      );
+
   MapUpdate : process(CLOCK, RESET_N)
-    variable selected_cell : map_cell_type;    
+    variable selected_cell : map_cell_type;
   begin
 
     if (RESET_N = '0') then
 
+      candy_removal <= '0';
+
       for i in 0 to MAP_ROWS-1 loop
         for j in 0 to MAP_COLUMNS-1 loop
-          -- TODO: logica incredibile per creazione mappa
+        -- TODO: logica incredibile per creazione mappa
         end loop;
       end loop;
 
     -- L'aggiornamento della mappa è sincrono con il clock di sistema  
     elsif (rising_edge(CLOCK)) then
       if (candy_removal = '1') then
-        selected_cell := map_board(REMOVE_CANDY.row, REMOVE_CANDY.col);
-        selected_cell.is_candy := '0';
+        selected_cell                                 := map_board(REMOVE_CANDY.row, REMOVE_CANDY.col);
+        selected_cell.is_candy                        := '0';
         map_board(REMOVE_CANDY.row, REMOVE_CANDY.col) <= selected_cell;
-        candy_removal := '0';
-        end if;
+        candy_removal                                 <= '0';
+      end if;
     end if;
 
   end process MapUpdate;
 
-  
+
   QueryCell : process(QUERY_CELL, map_board)
     variable selected_cell : map_cell_type;
   begin
-    CELL_CONTENT.is_wall <= '0';
+    CELL_CONTENT.is_wall  <= '0';
     CELL_CONTENT.is_candy <= '0';
 
     selected_cell := map_board(QUERY_CELL.row, QUERY_CELL.col);
@@ -67,7 +82,7 @@ begin
   -- Segnala internamente al process sincrono di rimuovere la caramellina
   RemoveCandy : process(REMOVE_CANDY)
   begin
-    candy_removal := '1';
+    candy_removal <= '1';
   end process RemoveCandy;
 
 end architecture;
