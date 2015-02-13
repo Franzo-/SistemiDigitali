154b92703a7e2010324c8b1e7d89f0c40fb7e3f7
caeuser <caeuser@caeuser-VirtualBox.(none)>
2015-02-13 12:18:37 +0100

    modifiche pacman controller

--- Controller/pacman_controller.vhd
+++ Controller/pacman_controller.vhd
@@ -12,22 +12,22 @@ entity Pacman_controller is
       RESET_N : in std_logic;
 
       --segnali dei tasti direzionali
-      BUTTON_NORTH     : in  std_logic;
-      BUTTON_SOUTH    : in  std_logic;
-      BUTTON_EAST    : in  std_logic;
-      BUTTON_WEST   : in  std_logic;
+      BUTTON_UP     : in  std_logic;
+      BUTTON_DOWN    : in  std_logic;
+      BUTTON_RIGHT    : in  std_logic;
+      BUTTON_LEFT   : in  std_logic;
 
 
       --segnali che indicano se il pacman puo muoversi nella data direzione
-      CAN_MOVE_NORTH : in std_logic;
-      CAN_MOVE_SOUTH : in std_logic;
-      CAN_MOVE_EAST : in std_logic;
-      CAN_MOVE_WEST : in std_logic;
+      CAN_MOVE_UP : in std_logic;
+      CAN_MOVE_DOWN : in std_logic;
+      CAN_MOVE_RIGHT : in std_logic;
+      CAN_MOVE_LEFT : in std_logic;
       --segnali in uscita	 
-      MOVE_NORTH : out std_logic;
-      MOVE_SOUTH : out std_logic;
-      MOVE_EAST : out std_logic;
-      MOVE_WEST : out std_logic		
+      MOVE_UP : out std_logic;
+      MOVE_DOWN : out std_logic;
+      MOVE_RIGHT : out std_logic;
+      MOVE_LEFT : out std_logic		
       
     );
 
@@ -38,31 +38,31 @@ Pacman_controller : process(CLOCK,RESET_N)
 begin
 	
 	if(RESET_N = '0') then
-		MOVE_NORTH      <= '0';  --monoimpulsori
-		MOVE_SOUTH      <= '0';
-		MOVE_WEST       <= '0';
-		MOVE_EAST       <= '0';
+		MOVE_UP      <= '0';  --monoimpulsori
+		MOVE_DOWN      <= '0';
+		MOVE_LEFT       <= '0';
+		MOVE_RIGHT       <= '0';
 
 	elsif rising_edge(CLOCK) then
-		MOVE_NORTH      <= '0';  --monoimpulsori
-		MOVE_SOUTH      <= '0';
-		MOVE_WEST       <= '0';
-		MOVE_EAST       <= '0';
+		MOVE_UP      <= '0';  --monoimpulsori
+		MOVE_DOWN      <= '0';
+		MOVE_LEFT       <= '0';
+		MOVE_RIGHT       <= '0';
 
 	--ad ogni fronte positivo di ck verifico dove deve andare il pacman
 	--(ad ogni passo corrisponde la pressione di un tasto)
 
-		if(BUTTON_NORTH = '1' and CAN_MOVE_NORTH = '1' )
-			MOVE_NORTH <= '1';
+		if(BUTTON_UP = '1' and CAN_MOVE_UP = '1' )
+			MOVE_UP <= '1';
 
-		elsif(BUTTON_SOUTH = '1' and CAN_MOVE_SOUTH = '1' )
-			MOVE_SOUTH <= '1';
+		elsif(BUTTON_DOWN = '1' and CAN_MOVE_DOWN = '1' )
+			MOVE_DOWN <= '1';
 
-		elsif(BUTTON_EAST = '1' and CAN_MOVE_EAST = '1' )
-			MOVE_EAST <= '1';
+		elsif(BUTTON_RIGHT = '1' and CAN_MOVE_RIGHT = '1' )
+			MOVE_RIGHT <= '1';
 
-		elsif(BUTTON_WEST = '1' and CAN_MOVE_WEST = '1' )
-			MOVE_WEST <= '1';
+		elsif(BUTTON_LEFT = '1' and CAN_MOVE_LEFT = '1' )
+			MOVE_LEFT <= '1';
 		end if;
 	end if;
 end process;
