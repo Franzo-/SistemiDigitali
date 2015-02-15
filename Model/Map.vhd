a4618b1fbfb665078976c8e6bc48bed253bcb54a
Franzo- <filippo.franzoni@studio.unibo.it>
2015-02-15 15:06:42 +0100

    Map: i processi 'passa carte' sono diventati statements concorrenti

--- Model/Map.vhd
+++ Model/Map.vhd
@@ -9,13 +9,13 @@ entity MapEntity is
       CLOCK           : in  std_logic;
       RESET_N         : in  std_logic;
       QUERY_NEARBY    : in  cell_nearby_array;
-      QUERY_CANDY : in cell_coordinates;
-      QUERY_VIEW : in cell_coordinates;
+      QUERY_CANDY     : in  cell_coordinates;
+      QUERY_VIEW      : in  cell_coordinates;
       REMOVE_CANDY    : in  cell_coordinates;
       --
       RESPONSE_NEARBY : out cell_nearby_content_array;
-      RESPONSE_CANDY : out map_cell_type;
-      RESPONSE_VIEW : out map_cell_type;
+      RESPONSE_CANDY  : out map_cell_type;
+      RESPONSE_VIEW   : out map_cell_type;
       CANDY_LEFT      : out integer range 0 to (MAX_CANDIES-1)
       );
 
@@ -66,36 +66,29 @@ begin
 
   -----------------------------------------------------------------------------
 
-  -- Aggiorna per tutti i personaggi
-  QueryNearby : process(QUERY_NEARBY, map_board)
-  begin
-
-    eachCharacter : for character in QUERY_NEARBY'range loop
-
-      RESPONSE_NEARBY(character).cell_up_content    <= map_board(QUERY_NEARBY(character).cell_up.row, QUERY_NEARBY(character).cell_up.col);
-      RESPONSE_NEARBY(character).cell_down_content  <= map_board(QUERY_NEARBY(character).cell_down.row, QUERY_NEARBY(character).cell_down.col);
-      RESPONSE_NEARBY(character).cell_left_content  <= map_board(QUERY_NEARBY(character).cell_left.row, QUERY_NEARBY(character).cell_left.col);
-      RESPONSE_NEARBY(character).cell_right_content <= map_board(QUERY_NEARBY(character).cell_right.row, QUERY_NEARBY(character).cell_right.col);
-
-    end loop eachCharacter;
-
-  end process QueryNearby;
-
-  -----------------------------------------------------------------------------
-
-  -- Risposta a query su singola cella
-  SingleCellQuery: process (QUERY_CANDY, QUERY_VIEW) is
-  begin
-    RESPONSE_CANDY <= map_board(QUERY_CANDY.row, QUERY_CANDY.col);
-    RESPONSE_VIEW <=  map_board(QUERY_VIEW.row, QUERY_VIEW.col);
-  end process SingleCellQuery;
-
-  -----------------------------------------------------------------------------
-
   -- Segnala internamente al process sincrono di rimuovere la caramellina
   RemoveCandy : process(REMOVE_CANDY)
   begin
     candy_removal <= '1';
   end process RemoveCandy;
+
+  -----------------------------------------------------------------------------
+
+  -- Aggiorna per tutti i personaggi con statements concorrenti
+  eachCharacter : for character in QUERY_NEARBY'range generate
+
+    RESPONSE_NEARBY(character).cell_up_content    <= map_board(QUERY_NEARBY(character).cell_up.row, QUERY_NEARBY(character).cell_up.col);
+    RESPONSE_NEARBY(character).cell_down_content  <= map_board(QUERY_NEARBY(character).cell_down.row, QUERY_NEARBY(character).cell_down.col);
+    RESPONSE_NEARBY(character).cell_left_content  <= map_board(QUERY_NEARBY(character).cell_left.row, QUERY_NEARBY(character).cell_left.col);
+    RESPONSE_NEARBY(character).cell_right_content <= map_board(QUERY_NEARBY(character).cell_right.row, QUERY_NEARBY(character).cell_right.col);
+
+  end generate eachCharacter;
+
+  -----------------------------------------------------------------------------
+
+  -- Risposta a query su singola cella con statements concorrenti
+  RESPONSE_CANDY <= map_board(QUERY_CANDY.row, QUERY_CANDY.col);
+  RESPONSE_VIEW  <= map_board(QUERY_VIEW.row, QUERY_VIEW.col);
+
 
 end architecture;
